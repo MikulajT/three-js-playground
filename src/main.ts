f0501f42a9ada6d7f43d77018e72f897e55bb856
Tomas Mikulaj <mikulajt@gmail.com>
2025-10-28 19:54:21 +0100

    Use responsive design

--- src/main.ts
+++ src/main.ts
@@ -54,6 +54,12 @@ const cubes = [
 function render(time: number) {
 		time *= 0.001; // convert time to seconds
 
+    if (resizeRendererToDisplaySize(renderer)){
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
 		cubes.forEach( ( cube, ndx ) => {
 
 			const speed = 3 + ndx * .1;
@@ -68,6 +74,17 @@ function render(time: number) {
 		requestAnimationFrame( render );
 }
 requestAnimationFrame(render);
+
+function resizeRendererToDisplaySize(renderer : THREE.WebGLRenderer) {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
 
 /// Line
 // const renderer = new THREE.WebGLRenderer();
